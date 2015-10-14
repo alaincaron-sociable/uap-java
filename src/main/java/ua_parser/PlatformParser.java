958dc74b0240d3dc5d55f697d7b60ee69873d056
Alain Caron <alain.caron@sociablelabs.com>
2015-10-14 13:39:32 -0400

    Precompiled some regexp and fix wrong string manipulation

--- src/main/java/ua_parser/PlatformParser.java
+++ src/main/java/ua_parser/PlatformParser.java
@@ -6,19 +6,29 @@
  */
 package ua_parser;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.Map;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PlatformParser {
-    private final Map<String, String> mobileDevicePatters;
-    private final Map<String, String> tabletDevicePatters;
+    private final Map<String, Pattern> mobileDevicePatters;
+    private final Map<String, Pattern> tabletDevicePatters;
     private final Logger LOGGER = Logger.getLogger(PlatformParser.class.getName());
 
     public PlatformParser(Map<String, String> mobileDevicePatters, Map<String, String> tabletDevicePatters) {
-        this.mobileDevicePatters = mobileDevicePatters;
-        this.tabletDevicePatters = tabletDevicePatters;
+        this.mobileDevicePatters = toPatternMap(mobileDevicePatters);
+        this.tabletDevicePatters = toPatternMap(tabletDevicePatters);
+    }
+
+    private static Map<String, Pattern> toPatternMap(Map<String, String> stringPatternMap) {
+        final ImmutableMap.Builder<String, Pattern> builder = ImmutableMap.<String, Pattern>builder();
+        for (Map.Entry<String,String> entry : stringPatternMap.entrySet()) {
+            builder.put(entry.getKey(), Pattern.compile(entry.getValue()));
+        }
+        return builder.build();
     }
 
     public Platform parse(String uaString) {
@@ -44,9 +54,9 @@ public class PlatformParser {
         return runPatternsOnString(tabletDevicePatters, ua);
     }
 
-    private boolean runPatternsOnString(Map<String, String> patterns, String input) {
-        for (Map.Entry<String, String> entry : patterns.entrySet()) {
-            final Pattern pattern = Pattern.compile(entry.getValue());
+    private boolean runPatternsOnString(Map<String, Pattern> patterns, String input) {
+        for (Map.Entry<String, Pattern> entry : patterns.entrySet()) {
+            final Pattern pattern = entry.getValue();
             final Matcher matcher = pattern.matcher(input);
 
             if (matcher.find()) {
